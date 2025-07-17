64bae10d1e0f4d15413dac0996e57278716ee717
Ayush <93836508+RYUKxAYU@users.noreply.github.com>
2025-07-17 23:09:50 +0530

    Time: 1131 ms (83.61%) | Memory: 18.1 MB (76.23%) - LeetSync

--- 3491-find-the-maximum-length-of-valid-subsequence-ii/find-the-maximum-length-of-valid-subsequence-ii.py
+++ 3491-find-the-maximum-length-of-valid-subsequence-ii/find-the-maximum-length-of-valid-subsequence-ii.py
@@ -1,10 +1,13 @@
 class Solution:
     def maximumLength(self, nums: list[int], k: int) -> int:
-        dp = [[0] * k for _ in range(k)]  
-        maxLength = 0
-        for num in nums:
-            current_rem = num % k
-            for prev_rem in range(k):
-                dp[prev_rem][current_rem] = dp[current_rem][prev_rem] + 1
-                maxLength = max(maxLength, dp[prev_rem][current_rem])
-        return maxLength
+        max_len=2
+        for targ_mod in range(k):
+            rem_dp=[0]*k
+
+            for num in nums:
+                num_mod=num%k
+                req_mod=(targ_mod-num_mod+k)%k
+                rem_dp[num_mod]= rem_dp[req_mod]+1
+
+            max_len=max(max_len,max(rem_dp))
+        return max_len
