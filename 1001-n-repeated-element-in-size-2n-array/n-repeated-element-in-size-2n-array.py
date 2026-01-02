7afaadc2294dd0a288989b8d97f9113355212811
Ayush <93836508+RYUKxAYU@users.noreply.github.com>
2026-01-02 23:34:16 +0530

    Time: 11 ms (36.33%) | Memory: 18.6 MB (61.16%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.py
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.py
@@ -1,12 +1,4 @@
 class Solution:
     def repeatedNTimes(self, nums: List[int]) -> int:
-        # Create an empty set to track seen elements
-        seen=set()
-        # Iterate through each element in nums:
-        for x in nums:
-            # Check if the element is already in the set
-            if x in seen:
-                return x
-            # If not seen before, add it to the set    
-            seen.add(x)
+        return mode(nums)
         
