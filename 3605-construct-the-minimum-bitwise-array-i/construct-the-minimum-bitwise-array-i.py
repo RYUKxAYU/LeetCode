dd1535b02a76ece6807873179f9071758dee51b1
Ayush <93836508+RYUKxAYU@users.noreply.github.com>
2026-01-20 23:28:50 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (20.59%) - LeetSync

--- 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.py
+++ 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.py
@@ -1,13 +1,16 @@
 class Solution:
     def minBitwiseArray(self, nums: List[int]) -> List[int]:
-        res=[]
-        for num in nums:
-            real=num
-            candi=-1
-            for j in range(1,real):
-                if(j|(j+1))==real:
-                    candi=j
-                    break
-            res.append(candi)
-        return res
+#bitwise approach= observation= ans| ans+1 changes the lowest 0 bit of ans to 1...
+#implication= nums[i] is essentially ans with one specific zero bit flipped to a one
+#to find ans from nums[i] we will do a flip 1 bit back to 0-> which to flip is to flip the max(of bit)
+#for edge case if num==2 we return -1
+        for i in range(len(nums)):
+            res=-1
+            d=1
+            while(nums[i]& d)!=0:
+                res=nums[i]-d
+                d<<=1
+            nums[i]=res
+        return nums
+
         
