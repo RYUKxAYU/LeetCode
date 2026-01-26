83d188149a796fc90fd81eaba777f5b5a2c76096
Ayush <93836508+RYUKxAYU@users.noreply.github.com>
2026-01-26 22:08:29 +0530

    Time: 47 ms (79.52%) | Memory: 31.4 MB (29.60%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.py
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.py
@@ -1,14 +1,5 @@
 class Solution:
     def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
         arr.sort()
-        n=len(arr)
-        min_diff=2e6+1
-        res=[]
-        for i in range(1,n):
-            diff=arr[i]-arr[i-1]
-            if diff<min_diff:
-                min_diff=diff
-                res=[[arr[i-1],arr[i]]]
-            elif diff==min_diff:
-                res.append([arr[i-1],arr[i]])
-        return res
+        min_diff=min(b-a for a,b in zip(arr,arr[1:]))
+        return [[a,b] for a,b in zip(arr,arr[1:]) if b - a ==min_diff]
