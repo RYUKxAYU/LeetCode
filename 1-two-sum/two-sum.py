1547dd39ef73c4567a31e994fd6208dede1c6ad0
Ayush <93836508+RYUKxAYU@users.noreply.github.com>
2026-02-20 23:22:56 +0530

    Time: 2 ms (55.06%) | Memory: 20.6 MB (15.98%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,22 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        nMap={}
-        n=len(nums)
-        for i in range(n):
-            nMap[nums[i]]=i
-        
-        for i in range(n):
-            complement=target-nums[i]
-            if complement in nMap and nMap[complement]!=i:
-                return[i,nMap[complement]]
-        return[]
-
-        # for i in range len(nums):
-        #     comp=target-nums[i] 
-        #     if comp not in num:
-        #         num.append(nums[i])
-        #         return 
-            
-
-
-        
+        dick={}
+        for i,num in enumerate(nums):
+            comple=target-num
+            if comple in dick:
+                return[dick[comple],i]
+            dick[num]=i
+        return []
+         
