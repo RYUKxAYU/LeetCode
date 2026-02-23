892e245a3eafe703eda3d5d5f0ee2c3282850e05
Ayush <93836508+RYUKxAYU@users.noreply.github.com>
2026-02-23 15:26:34 +0530

    Time: 2 ms (44.40%) | Memory: 19.5 MB (21.72%) - LeetSync

---
+++ 202-happy-number/happy-number.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        def gnn(n):
+            output=0
+            while n:
+                digit=n%10
+                output+=digit**2
+                n=n//10
+            return output
+
+        slow=gnn(n)
+        fast=gnn(gnn(n))
+
+        while slow !=fast:
+            if fast ==1: return True
+            slow=gnn(slow)
+            fast=gnn(gnn(fast))
+        
+        return slow==1
+        
