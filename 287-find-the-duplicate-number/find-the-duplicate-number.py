50c79c607dd694ec358a4bcb2f7fca255292e8c2
Ayush <93836508+RYUKxAYU@users.noreply.github.com>
2026-02-26 20:34:02 +0530

    Time: 968 ms (5.06%) | Memory: 33.6 MB (60.07%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,7 +1,24 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        seen = {}
-        for num in nums:
-            if num in seen:
-                return num
-            seen[num] = True
+        n = len(nums) - 1
+        duplicate = 0
+        for bit in range(32):
+            mask = (1 << bit)
+            count_nums = 0
+            count_range = 0
+            
+            for i in range(len(nums)):
+                # Count bits in the actual array
+                if nums[i] & mask:
+                    count_nums += 1
+                
+                # Count bits in the range 1 to n
+                if i > 0 and (i & mask):
+                    count_range += 1
+            
+            # If the bit is more frequent in the array, 
+            # it belongs to our duplicate number.
+            if count_nums > count_range:
+                duplicate |= mask
+                
+        return duplicate
